b1ccd2e367e585772efe9ecf9341e1a3b635a671
TaironeNzz <taironejw@gmail.com>
2025-11-09 21:12:14 -0300

    att

---
+++ src/main/java/model/RoboExplorador.java
@@ -0,0 +1,21 @@
+package model;
+
+import javafx.scene.image.Image;
+
+public class RoboExplorador extends Robo{
+    /**
+     * Construtor da classe, define as posições, o destino, se o robô está em movimento, carrega os sprites e define o sprite inicial.
+     * @param posicaoX
+     * @param posicaoY
+     */
+    public RoboExplorador(int posicaoX, int posicaoY) {
+        setPosicaoX(posicaoX);
+        setPosicaoY(posicaoY);
+        setDestinoX(posicaoX);
+        setDestinoY(posicaoY);
+        setSprite1(new Image(getClass().getResourceAsStream("/sprites/robo-verde-1.png")));
+        setSprite2(new Image(getClass().getResourceAsStream("/sprites/robo-verde-2.png")));
+        setSpriteAtual(getSprite1());
+    }
+
+}
